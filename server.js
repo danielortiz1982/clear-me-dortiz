bea17c8d748d3088f588eed3205fe641f942697d
danielortiz1982 <danielortiz1982@gmail.com>
2020-08-09 15:20:42 -0400

    update to server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ server.use(express.json())
 const OrganizationRouter = require('./router/organization-router')
 server.use(OrganizationRouter)
 
+server.get('/', (req, res) => {
+   res.status(200).send('<h1>Clear Me Demo API</h1>')
+})
+
 
 if(!module.parent) {
     server.listen('3500', () => console.log('Server running on post 3500'))
