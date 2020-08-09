d31df4b9d66c6e47d69b2fef11a65da3a67e67c6
danielortiz1982 <danielortiz1982@gmail.com>
2020-08-09 17:25:21 -0400

    first commit

--- utilities/db-connect.js
+++ utilities/db-connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const database  = 'clear-me-api'
 const appPort   = 27017
-const dbConnection = `mongodb://danielortiz1982:L3in@d1982@ds157895.mlab.com:57895/clear-me-api`
+const dbConnection = `mongodb://danielortiz1982:l3inad1982@ds157895.mlab.com:57895/clear-me-api`
 
 mongoose.connect(dbConnection, {
     useNewUrlParser: true,
